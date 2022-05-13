0085d040a6ab27b86557920ad389e873ddded8f9
Vladislav Surin <vlad_surin@mail.ru>
2022-05-13 10:47:41 +0300

    initial commit

---
+++ guessing_game.js
@@ -0,0 +1,32 @@
+let answer = parseInt(Math.random() * 100);
+
+let playerNumber = 1;
+
+while(true){ //false - ложь
+
+    let userAnswer = prompt("Угадайте число от 1 до 100.\nХодит игрок " + playerNumber + ".\nДля выхода нажмите q");
+
+    if(userAnswer == 'q') {
+        break;
+    }
+
+    userAnswer = parseInt(userAnswer);
+
+    if(userAnswer == answer){
+        alert("Поздравляю! Победил игрок " + playerNumber);
+        break;
+        // continue; - сразу переходим к следующей итерации
+    } else if( userAnswer > answer) {
+        alert("Вы ввели слишком большое число");
+    } else if(userAnswer < answer) {
+        alert("Вы ввели слишком маленькое число");
+    }
+
+    if(playerNumber == 1){
+        playerNumber = 2;
+    } else {
+        playerNumber = 1;
+    }
+}
+
+alert("Праильный ответ: " + answer);
